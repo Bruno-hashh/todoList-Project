b63244b6b3e579cae83ea95ee3ec6a344e1a369a
Bruno Pires <130810105+Bruno-hashh@users.noreply.github.com>
2023-07-05 17:34:07 -0400

    Começo de Upgrade

---
+++ src/pages/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <p class="text-h1">TODO-LIST</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
